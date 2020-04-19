a1f9a9120748a9112abbf98e6d32407cda0bef4c
derin <derin.mathew@attinadsoftware.com>
2020-04-20 00:34:42 +0530

    Initial commit

---
+++ build/precache-manifest.a5e6c862f5992eb54016bc792df28904.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "159088f0b5b26bbb4745f47b613baf74",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4596657b9cbdaab65380",
+    "url": "/static/css/main.74e8d976.chunk.css"
+  },
+  {
+    "revision": "ea201a2a243d203fe55e",
+    "url": "/static/js/2.8d253b65.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.8d253b65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4596657b9cbdaab65380",
+    "url": "/static/js/main.7b2ba844.chunk.js"
+  },
+  {
+    "revision": "c7cab0e691400c070e9f",
+    "url": "/static/js/runtime-main.f13a553e.js"
+  }
+]);
